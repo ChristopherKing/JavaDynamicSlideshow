8ebf771f7ba3d235da91304a16c621ff0827d874
Christopher King <cking4@iit.edu>
2012-05-07 12:34:43 -0500

    Got rid of that bad code from example.

--- Dynamic Slideshow/src/dynamic/slideshow/DynamicSlideshow.java
+++ Dynamic Slideshow/src/dynamic/slideshow/DynamicSlideshow.java
@@ -6,16 +6,8 @@ package dynamic.slideshow;
 
 
 
-import java.awt.Graphics; 
-import java.awt.GraphicsEnvironment; 
-import java.awt.Image; 
-import java.awt.Toolkit; 
-import java.awt.event.MouseEvent; 
-import java.awt.event.MouseListener; 
-import java.awt.event.WindowAdapter; 
-import java.awt.event.WindowEvent; 
-import java.net.MalformedURLException; 
-import java.net.URL; 
+import java.awt.*;
+import javax.swing.ImageIcon;
 
 import java.io.*;
  
@@ -40,57 +32,7 @@ public class DynamicSlideshow extends JFrame {
  
     // Program entry 
     public static void main(String[] args) throws Exception { 
-        if (args.length < 1) //load test images directory by default
-            new DynamicSlideshow("C:\testimages"); 
-        else {
-        }
-            new DynamicSlideshow(args[0]); // or first command-line argument 
-    } 
- 
-    // Class constructor  
-    DynamicSlideshow(String source) throws MalformedURLException { 
- 
-        this.folder = source;
-        this.modified = 0L;
-        // Exiting program on window close 
-        addWindowListener(new WindowAdapter() { 
-            public void windowClosing(WindowEvent e) { 
-                System.exit(0);
-            } 
-        });
- 
-        // Exitig program on mouse click 
-        addMouseListener(new MouseListener() { 
-            public void mouseClicked(MouseEvent e) { System.exit(0); } 
-            public void mousePressed(MouseEvent e) {} 
-            public void mouseReleased(MouseEvent e) {} 
-            public void mouseEntered(MouseEvent e) {} 
-            public void mouseExited(MouseEvent e) {} 
-        } 
-        );
- 
-        // remove window frame  
-        this.setUndecorated(true);
- 
-        // window should be visible 
-        this.setVisible(true);
- 
-        // switching to fullscreen mode 
-        GraphicsEnvironment.getLocalGraphicsEnvironment().
-        getDefaultScreenDevice().setFullScreenWindow(this);
- 
-        // getting display resolution: width and height 
-        w = this.getWidth();
-        h = this.getHeight();
-        System.out.println("Display resolution: " + String.valueOf(w) + "x" + String.valueOf(h));
- 
-        //load images
-        this.slideshow = loadImages(null);
-        //
-        if (source.startsWith("http://")) // http:// URL was specified 
-            screenImage = Toolkit.getDefaultToolkit().getImage(new URL(source));
-        else 
-            screenImage = Toolkit.getDefaultToolkit().getImage(source); // otherwise - file 
+        
     } 
     
     /*
@@ -146,17 +88,6 @@ public class DynamicSlideshow extends JFrame {
         
         return longTime;
     }
- 
-    public void paint (Graphics g) { 
-        if (screenImage != null) // if screenImage is not null (image loaded and ready) 
-            g.drawImage(screenImage, // draw it  
-                        w/2 - screenImage.getWidth(this) / 2, // at the center  
-                        h/2 - screenImage.getHeight(this) / 2, // of screen 
-                        this);
-            // to draw image at the center of screen 
-            // we calculate X position as a half of screen width minus half of image width 
-            // Y position as a half of screen height minus half of image height 
-    } 
     
     public class OnlyImage implements FilenameFilter {
         
