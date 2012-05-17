a7215c1a7ea46343abe6f8cd7a718eaf3517af52
Christopher King <cking4@iit.edu>
2012-05-17 16:55:22 -0500

    Lots of change. Use BufferedImages now instead of toolkit images. Slideshow works now. Rotates and everything.

--- Dynamic Slideshow/src/dynamic/slideshow/DynamicSlideshow.java
+++ Dynamic Slideshow/src/dynamic/slideshow/DynamicSlideshow.java
@@ -5,10 +5,11 @@
 package dynamic.slideshow;
 
 import java.awt.*;
-import javax.swing.ImageIcon;
-
-import java.io.*;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 /**
@@ -19,12 +20,17 @@ public class DynamicSlideshow extends JFrame {
 
     // this line is needed to avoid serialization warnings  
     private static final long serialVersionUID = 1L;
-    Image screenImage; // downloaded image  
-    Image[] slideshow;
-    int w, h; // Display height and width 
+    private BufferedImage[] slideshow;
+    private int w, h; // Display height and width 
     private static final String folder = "C:\\testimages";
-    long modified;
-
+    private long modified;
+    private static final int FONT_SIZE = 24;
+    private static final long TRANSITION_TIME = 5000;
+    private SimpleScreenManager screen;
+    private int currentSlide;
+    private boolean imagesLoaded;
+    private boolean go;
+    
     // Program entry 
     public static void main(String[] args) throws Exception {
         DisplayMode displayMode;
@@ -42,13 +48,7 @@ public class DynamicSlideshow extends JFrame {
         DynamicSlideshow test = new DynamicSlideshow();
         test.run(displayMode);
     }
-    private static final int FONT_SIZE = 24;
-    private static final long TRANSITION_TIME = 5000;
-    private SimpleScreenManager screen;
-    private Image drawnImage;
-    private int currentSlide;
-    private boolean imagesLoaded;
-    private boolean go;
+
 
     public void run(DisplayMode displayMode) {
         setBackground(Color.blue);
@@ -62,77 +62,47 @@ public class DynamicSlideshow extends JFrame {
         try {
             screen.setFullScreen(displayMode, this);
             go = true;
-            while(go) {
-                loadSlide(); //load the first image
-                /*try {
-                    Thread.sleep(TRANSITION_TIME);
-                } catch (InterruptedException ex) {
-                }*/
+            while (go) {
+                //loadSlide(); //load the first image
+                /*
+                 * try { Thread.sleep(TRANSITION_TIME); } catch
+                 * (InterruptedException ex) { }
+                 */
+                //drawnImage = slideshow[0];
+                repaint();
                 waiting(5);
-                
+                //drawnImage.flush();
+                //drawnImage = slideshow[1];
+                currentSlide++; //advance the slide
+
                 //if we just displayed the last image check if the images changed
-                if(currentSlide == slideshow.length-1) {
+                if (currentSlide == slideshow.length - 1) {
                     slideshow = getImages(slideshow); //we give old slideshow as input to check against
+                    currentSlide = 0; //reset slide counter because we were at the end
                 }
             }
         } finally {
             screen.restoreScreen();
         }
     }
-    
-    public static void waiting (int n){
-        
+
+    //wait a specified amount of time
+    public static void waiting(int n) {
+
         long t0, t1;
 
-        t0 =  System.currentTimeMillis();
+        t0 = System.currentTimeMillis();
 
-        do{
+        do {
             t1 = System.currentTimeMillis();
-        }
-        while ((t1 - t0) < (n * 1000));
-    }
-
-    /*
-     * This method changes the current slide and sets the drawnImage class
-     * variable to the image that is to be displayed next. It then signals that
-     * the image is loaded and signals the graphics engine to repaint the window.
-     * It also advances the slide counter or resets it to 0 so it will continuously
-     * display the images.
-     */
-    public void loadSlide() {
-        //bgImage = loadImage("images/background.jpg");
-        //drawnImage = loadImage("C:\\testimages\\map.JPG");
-        //transparentImage = loadImage("images/transparent.png");
-        //translucentImage = loadImage("images/translucent.png");
-        //antiAliasedImage = loadImage("images/antialiased.png");
-        
-        if(drawnImage != null)
-            drawnImage.flush();
-        drawnImage = slideshow[currentSlide];
-        currentSlide++;
-        if(currentSlide >= slideshow.length)
-            currentSlide = 0;
-        imagesLoaded = true;
-        // signal to AWT to repaint this window
-        repaint();
+        } while ((t1 - t0) < (n * 1000));
     }
 
     public void paint(Graphics g) {
-        // set text anti-aliasing
-        /*if (g instanceof Graphics2D) {
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setRenderingHint(
-                    RenderingHints.KEY_TEXT_ANTIALIASING,
-                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        }*/
-
         // draw images
         if (imagesLoaded) {
-            //g.drawImage(bgImage, 0, 0, null);
-            drawImage(g, drawnImage, 0, 0, "Image " + currentSlide);
-            //drawImage(g, transparentImage, 320, 0, "Transparent");
-            //drawImage(g, translucentImage, 0, 300, "Translucent");
-            //drawImage(g, antiAliasedImage, 320, 300, "Translucent (Anti-Aliased)");
+            //drawImage(g, slideshow[currentSlide], 0, 0, "Image " + currentSlide);
+            g.drawImage(slideshow[currentSlide], 0, 0, null);
         } else {
             g.drawString("Loading Images...", 5, FONT_SIZE);
         }
@@ -150,11 +120,11 @@ public class DynamicSlideshow extends JFrame {
      * current modification timstamps in the folder are more recent than the
      * stored one all the images in the folder will be reloaded.
      */
-    private Image[] getImages(Image[] oldImages) {
+    private BufferedImage[] getImages(BufferedImage[] oldImages) {
         //load folder
         File datFolder = new File(folder);
-        File[] imageFiles = datFolder.listFiles(new OnlyImage());
-        Image[] images = null;
+        File[] imageFiles = datFolder.listFiles(new OnlyImage()); //load images
+        BufferedImage[] images = null;
         //if the folder is empty
         if (imageFiles == null) {
             System.out.println("The folder was empty or did not contain images.");
@@ -163,12 +133,25 @@ public class DynamicSlideshow extends JFrame {
         else {
             //check modified timestamps
             //if the new file list has been modified since last update then reload images
-            if (getLatestModified(imageFiles) > this.modified) {
-                images = new Image[imageFiles.length];
+            long temp = getLatestModified(imageFiles); //store time in temp var so files are only accessed once
+            if (temp > this.modified) {
+                modified = temp; //store new modified time
+                
+                //flush old images out of memory
+                for (int i = 0; i < images.length; i++) {
+                    images[i].flush();
+                }
+                images = new BufferedImage[imageFiles.length]; //new array
                 //populate image array with new images from file list in files array
                 for (int i = 0; i < imageFiles.length; i++) {
-                    images[i] = Toolkit.getDefaultToolkit().createImage(imageFiles[i].getAbsolutePath());
+                    try {
+                        images[i] = ImageIO.read(imageFiles[i]);
+                    } catch (IOException e) {
+                        System.err.println("Caught IOException: " + e.getMessage());
+                        System.exit(1);
+                    }
                 }
+                imagesLoaded = true;
                 return images;
             } //otherwise continue using old images
             else {
@@ -176,38 +159,42 @@ public class DynamicSlideshow extends JFrame {
             }
 
         }
-        return images;
+        return null; //program will never get here
     }
-    
-    private Image[] getImages() {
+
+    private BufferedImage[] getImages() {
         //load folder
         File datFolder = new File(folder);
-        File[] imageFiles = datFolder.listFiles(new OnlyImage());
-        Image[] images = null;
+        File[] imageFiles = datFolder.listFiles(new OnlyImage()); //load array with all image files found in folder
+        BufferedImage[] images = null;
         //if the folder is empty
         if (imageFiles == null) {
             System.out.println("The folder was empty or did not contain images.");
             System.exit(0);
         } //otherwise it has something in it
         else {
-            //check modified timestamps
-            //if the new file list has been modified since last update then reload images
-
-            images = new Image[imageFiles.length];
+            //create an array to hold the images
+            images = new BufferedImage[imageFiles.length];
             //populate image array with new images from file list in files array
             for (int i = 0; i < imageFiles.length; i++) {
-                //images[i] = loadImage(imageFiles[i].getAbsolutePath());
-                images[i] = Toolkit.getDefaultToolkit().createImage(imageFiles[i].getAbsolutePath());
+                try {
+                    images[i] = ImageIO.read(imageFiles[i]); //create BufferedImages
+                } catch (IOException e) {
+                    System.err.println("Caught IOException: " + e.getMessage());
+                    System.exit(1);
+                }
             }
+            imagesLoaded = true;
+            modified = getLatestModified(imageFiles); //set modified time to be checked on next update
             return images;
 
 
         }
-        return images;
+        return null; //the program will never get here as imageFiles will either be null or not null.
     }
 
     //Should return a long int representing the most recently modified file's modification time
-    public static long getLatestModified(File[] files) {
+    public long getLatestModified(File[] files) {
         long longTime = 0L;
         if (files == null) {
             return 0L;
